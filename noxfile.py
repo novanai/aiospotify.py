919008f2727e6b62e03044a076ef6895b57650bc
nova <novanai.dev@gmail.com>
2024-07-25 13:15:12 +0100

    Update noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,34 +1,26 @@
 import nox
 
-# Dependencies must be set to an absolute version for this to work
-project = nox.project.load_toml("pyproject.toml")["project"]
-
-deps: dict[str, dict[str, str]] = {
-    group: {dep.split("==")[0]: dep for dep in deps}
-    for group, deps in project["optional-dependencies"].items()
-}
-
 
 @nox.session
 def format(session: nox.Session) -> None:
-    session.install(deps["dev"]["ruff"])
+    session.install(".[dev.ruff]")
     session.run("ruff", "check", "spotify", "--select", "I", "--fix")
     session.run("ruff", "format", "spotify")
 
 
 @nox.session
 def typing(session: nox.Session) -> None:
-    session.install(deps["dev"]["pyright"], *project["dependencies"])
+    session.install(".[dev.typecheck]")
     session.run("pyright", "spotify")
 
 
 @nox.session
 def docs_build(session: nox.Session) -> None:
-    session.install(*deps["docs"].values())
+    session.install(".[dev.docs]")
     session.run("mkdocs", "build")
 
 
-@nox.session
+@nox.session(default=False)
 def docs_serve(session: nox.Session) -> None:
-    session.install(*deps["docs"].values())
+    session.install(".[dev.docs]")
     session.run("mkdocs", "serve")
